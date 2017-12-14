e3e8918f30675c9b1d2119453218b47f24b30181
Fadel Kaadan <fadel95kaadan@gmail.com>
2017-12-14 12:42:19 +0100

    vorhersagen() und kartelegen() sind jetzt fertig

--- src/Bot.java
+++ src/Bot.java
@@ -16,24 +16,32 @@ public class Bot extends Spieler {
 	@Override
 	public Karte karteLegen() {
 		Karte tempKarte = null;
-		for (int i = 0; i < Wizzard.runde; i++) {
-			if (handKarten[i].getFarbe() == Wizzard.kartenfeld[Wizzard.aktiverSpieler].getFarbe()
-					&& Wizzard.kartenfeld[Wizzard.aktiverSpieler] != null) {
-				tempKarte = handKarten[i];
-				handKarten[i] = null;
-				System.out.println(this.getName()+" "+tempKarte);
-				return tempKarte;
-
+		if (this == Wizzard.spieler[Wizzard.aktiverSpieler]) {
+			for (int i = 0; i < Wizzard.runde; i++) {
+				if (handKarten[i] != null) {
+					tempKarte = handKarten[i];
+					handKarten[i] = null;
+					System.out.println(this.getName()+" legt "+tempKarte);
+					return tempKarte;
+				}
+			}
+		} else {
+			for (int i = 0; i < Wizzard.runde; i++) {
+				if (handKarten[i] != null) {
+					if (handKarten[i].getFarbe() == Wizzard.kartenfeld[Wizzard.aktiverSpieler].getFarbe()) {
+						tempKarte = handKarten[i];
+						handKarten[i] = null;
+						System.out.println(this.getName()+" legt "+tempKarte);
+						return tempKarte;
+					} else {
+						tempKarte = handKarten[i];
+						handKarten[i] = null;
+						System.out.println(this.getName()+" legt "+tempKarte);
+						return tempKarte;
+					}
+				}
 			}
 		}
-		for (int i = 0; i < Wizzard.runde; i++) {
-			if (handKarten[i] != null) {
-				tempKarte = handKarten[i];
-				handKarten[i] = null;
-				return tempKarte;
-			}
-		}
-		System.out.println("meine karte: " + tempKarte);
 		return tempKarte;
 	}
 }
